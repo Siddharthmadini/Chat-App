381184a4bed6ed715bacfeb0bbe479bd936534be
Siddharthmadini <siddharthmadini2811@gmail.com>
2026-05-24 19:58:53 +0530

    Simplify CORS config, use plain origin array

--- server/server.js
+++ server/server.js
@@ -22,14 +22,7 @@ const allowedOrigins = (process.env.CLIENT_URL || process.env.FRONTEND_URL)
   : ['http://localhost:3000'];
 
 const corsOptions = {
-  origin: function (origin, callback) {
-    // Allow requests with no origin (mobile apps, curl, Postman)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) return callback(null, true);
-    // In development allow all
-    if (process.env.NODE_ENV !== 'production') return callback(null, true);
-    callback(null, false);
-  },
+  origin: allowedOrigins,
   credentials: true
 };
 
